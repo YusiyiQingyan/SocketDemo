ad6a65369d697454e61cc22d1940418a44cf4730
YusiyiQingyan <3032241396@qq.com>
2020-04-07 18:16:22 +0800

    初次提交，利用socket完成最基本的B/S通信

---
+++ src/socket/Client.java
@@ -0,0 +1,52 @@
+package socket;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Created by weiwen on 2020/4/7
+ */
+
+public class Client {
+    public static void main(String[] args) {
+        final String DEFAULT_SERVER_HOST = "127.0.0.1";
+        final int DEFAULT_SERVER_PORT = 8888;
+        Socket socket = null;
+        BufferedWriter writer = null;
+
+        try {
+            //创建Socket
+            socket = new Socket(DEFAULT_SERVER_HOST, DEFAULT_SERVER_PORT);
+            //创建IO流
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            writer = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream()));
+
+            //等待用户从控制台输入待发送的消息
+            BufferedReader consoleReader = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+            String input = consoleReader.readLine();
+            //发送消息到服务器
+            writer.write(input + "\n");
+            writer.flush();
+            //读取服务端返回的消息
+            // 读取服务器返回的消息
+            String msg = reader.readLine();
+            System.out.println(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                    System.out.println("关闭socket");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
---
+++ src/socket/Server.java
@@ -0,0 +1,51 @@
+package socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by weiwen on 2020/4/7
+ */
+
+public class Server {
+
+    public static void main(String[] args) {
+        final int DEFAULT_PORT = 8888;
+        ServerSocket serverSocket = null;
+
+        try {
+            //绑定监听端口
+            serverSocket = new ServerSocket(DEFAULT_PORT);
+            System.out.println("服务开启，监听端口：" + DEFAULT_PORT);
+            //等待客户端连接
+            Socket socket = serverSocket.accept();
+            System.out.println("客户端[" + socket.getPort() + "]已连接");
+            //客户端和服务端进行IO操作
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream()));
+            //读取客户端发送的消息
+            String msg = reader.readLine();
+            if (msg != null) {
+                System.out.println("客户端[" + socket.getPort() + "]:" + msg);
+            }
+            //对客户端发来的消息给予响应
+            writer.write("服务器：" + msg + "\n");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                    System.out.println("关闭serverSocket");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
